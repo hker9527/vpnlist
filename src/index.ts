32a19075f44fa5ef44d04590deae0e37ecc6f67c
hker9527 <owo@uwu.cc>
2023-10-18 20:59:49 +0800

    Add `/api/server/:ip/config` route; Validate IP format

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { Router, createCors, error, json } from "itty-router";
+import { Router, createCors, createResponse, error, json, text } from "itty-router";
 import { SITES, Site } from "./const";
 import { buildClient, getClient } from "./drizzle/client";
 import { TestResultCache } from "./cache/TestResults";
 import { serverTable } from "./drizzle/schema";
 import { eq } from "drizzle-orm";
+import { isIPv4 } from "is-ip";
 
 export interface Env {
     LIBSQL_DB_URL: string;
@@ -42,17 +43,32 @@ const buildRouter = () => {
         .get("/api/server/:ip", async ({ params }) => {
             const ip = params.ip;
 
+            if (!isIPv4(ip)) {
+                return json({
+                    success: false,
+                    data: null
+                }, {
+                    status: 400
+                });
+            }
+
             const db = getClient();
 
             const data = await db.query.serverTable.findFirst({
                 columns: {
-                    asnId: false
+                    asnId: false,
+                    config: false
                 },
                 where: eq(serverTable.ip, ip)
             });
 
             if (!data) {
-                return error(404);
+                return json({
+                    success: false,
+                    data: null
+                }, {
+                    status: 404
+                });
             }
 
             return json({
@@ -60,8 +76,59 @@ const buildRouter = () => {
                 data
             });
         })
+        .get("/api/server/:ip/config", async ({ params }) => {
+            const ip = params.ip;
+
+            if (!isIPv4(ip)) {
+                return json({
+                    success: false,
+                    data: null
+                }, {
+                    status: 400
+                });
+            }
+            
+            const db = getClient();
+
+            const data = await db.query.serverTable.findFirst({
+                columns: {
+                    config: true
+                },
+                where: eq(serverTable.ip, ip)
+            });
+
+            if (!data) {
+                return json({
+                    success: false,
+                    data: null
+                }, {
+                    status: 404
+                });
+            }
+
+            const generateFileName = () => {
+                const now = new Date();
+                const year = now.getFullYear();
+                const month = now.getMonth() + 1;
+                const date = now.getDate();
+                const hour = now.getHours();
+                const minute = now.getMinutes();
+                const second = now.getSeconds();
+
+                return `NasuVPN-${year}-${month}-${date}-${hour}-${minute}-${second}-${data.ip}.ovpn`;
+            };
+
+            return createResponse("application/x-openvpn-profile")(data.config, {
+                headers: {
+                    "Content-Disposition": `attachment; filename="${generateFileName()}"`
+                }
+            });
+        }
+        )
         .all("*", () => {
-            return error(404);
+            return text("OwO?", {
+                status: 404
+            });
         });
 }
 
