c61fe87ea9dbe5d0d124ebbec2d44153b221407e
hker9527 <owo@uwu.cc>
2023-10-23 17:48:56 +0800

    Add site selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
-    import { onMount } from "svelte";
 	import Server from "~/lib/Server.svelte";
 	import Accordion from "@smui-extra/accordion";
     import { HOST } from "~/lib/const";
     import { ZSiteAPIResponse, type SiteResult } from "~/lib/types/api/SiteAPIResponse";
+    import Group from "@smui/button/src/Group.svelte";
+    import Button from "@smui/button/src/Button.svelte";
+    import { Label } from "@smui/button";
+    import { onMount } from "svelte";
 
-	let siteResult: SiteResult;
-	
-	onMount(async () => {
-		fetch(`${HOST}/api/site/uma`).then(async (res) => {
+	let siteResult: SiteResult | null = null;
+	let siteVariants: Record<string, "raised" | "outlined"> = {
+		"uma": "raised",
+		"dmm": "outlined"
+	};
+
+	const fetchResult = async (site: string) => {
+		siteResult = null;
+
+		fetch(`${HOST}/api/site/${site}`).then(async (res) => {
 			const json = await res.json()
 			if (ZSiteAPIResponse.check(json)) {
 				if (json.success) {
@@ -18,10 +27,38 @@
 				alert(ZSiteAPIResponse.reason(json));
 			}
 		});
+	};
+
+	const onClick = async (site: string) => {
+		if (siteVariants[site] === "raised") return;
+
+		for (const button in siteVariants) {
+			siteVariants[button] = "outlined";
+		}
+
+		siteVariants[site] = "raised";
+
+		fetchResult(site);
+	}
+
+	onMount(async () => {
+		await fetchResult("uma");
 	});
 </script>
 
 <main>
+	<Group class="my-4">
+		<Button variant={siteVariants["uma"]} class="site-button" on:click={(e) => onClick("uma")}>
+			<Label>
+				<img src="/uma.png" alt="" />
+			</Label>
+		</Button>
+		<Button variant={siteVariants["dmm"]} class="site-button" on:click={(e) => onClick("dmm")}>
+			<Label>
+				<img src="/dmm.png" alt="" />
+			</Label>
+		</Button>
+	</Group>
     <div class="accordion-container">
 		{#if siteResult}
 			<Accordion multiple>
@@ -42,5 +79,9 @@
 	main {
 		max-width: 1000px;
 		margin: 0 auto;
+	}
+
+	* :global(.site-button) {
+		height: 48px;
 	}
 </style>
