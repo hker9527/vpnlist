9a7b7d58ad34a7030a5e53821c3833148e12b5a6
hker9527 <owo@uwu.cc>
2023-10-25 22:04:57 +0800

    Add appbar and country filtering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,52 @@
 <script lang="ts">
-	import Server from "~/lib/Server.svelte";
 	import Accordion from "@smui-extra/accordion";
-    import { HOST } from "~/lib/const";
-    import { ZSiteAPIResponse, type SiteResult } from "~/lib/types/api/SiteAPIResponse";
-    import Group from "@smui/button/src/Group.svelte";
-    import Button from "@smui/button/src/Button.svelte";
-    import { Label } from "@smui/button";
-    import { onMount } from "svelte";
+	import Button, { Group, Icon, Label } from "@smui/button";
+	import Drawer, { AppContent, Content as DrawerContent } from "@smui/drawer";
+	import IconButton from "@smui/icon-button";
+	import LayoutGrid, { Cell } from "@smui/layout-grid";
+	import List, { Item, Text } from "@smui/list";
+	import Select, { Option } from "@smui/select";
+	import TopAppBar, {
+	    AutoAdjust,
+	    Row,
+	    Section,
+	    Title,
+	} from "@smui/top-app-bar";
+	import { onMount } from "svelte";
+	import Server from "~/lib/Server.svelte";
+	import { HOST } from "~/lib/const";
+	import { country2emoji } from "~/lib/emoji";
+	import {
+	    ZSiteAPIResponse,
+	    type SiteResult,
+	} from "~/lib/types/api/SiteAPIResponse";
 
-	let siteResult: SiteResult | null = null;
+	let topAppBar: TopAppBar;
+	let siteResult: {
+		value: SiteResult | null;
+		filters: {
+			country: string;
+		};
+	} = {
+		value: null,
+		filters: {
+			country: "",
+		},
+	};
+
 	const siteVariants: Record<string, "raised" | "outlined"> = {
-		"uma": "raised",
-		"dmm": "outlined"
+		uma: "raised",
+		dmm: "outlined",
 	};
 
 	const fetchResult = async (site: string) => {
-		siteResult = null;
+		siteResult.value = null;
 
 		fetch(`${HOST}/api/site/${site}`).then(async (res) => {
-			const json = await res.json()
+			const json = await res.json();
 			if (ZSiteAPIResponse.check(json)) {
 				if (json.success) {
-					siteResult = json.data;
+					siteResult.value = json.data;
 				}
 			} else {
 				alert(ZSiteAPIResponse.reason(json));
@@ -29,56 +54,115 @@
 		});
 	};
 
-	const onClick = async (site: string) => {
-		if (siteVariants[site] === "raised") return;
+	let site = "uma";
+	const onClick = async (_site: string) => {
+		site = _site;
 
+		if (siteVariants[site] === "raised") return;
 		for (const button in siteVariants) {
 			siteVariants[button] = "outlined";
 		}
-
 		siteVariants[site] = "raised";
 
 		await fetchResult(site);
-	}
+	};
 
 	onMount(async () => {
-		await fetchResult("uma");
+		await fetchResult(site);
 	});
+
+	const onChange = async () => {
+		await fetchResult(site);
+	};
+
+	let open = false;
 </script>
 
 <main>
-	<Group class="my-4">
-		<Button variant={siteVariants["uma"]} class="site-button" on:click={(e) => onClick("uma")}>
-			<Label>
-				<img src="/uma.png" alt="" />
-			</Label>
-		</Button>
-		<Button variant={siteVariants["dmm"]} class="site-button" on:click={(e) => onClick("dmm")}>
-			<Label>
-				<img src="/dmm.png" alt="" />
-			</Label>
-		</Button>
-	</Group>
-    <div class="accordion-container">
-		{#if siteResult}
-			<Accordion multiple>
-				{#each siteResult as result}
-					<Server result={result} />
+	<TopAppBar bind:this={topAppBar}>
+		<Row>
+			<Section>
+				<IconButton
+					class="material-icons"
+					on:click={() => (open = !open)}>menu</IconButton
+				>
+				<Title>NasuVPN Checker</Title>
+			</Section>
+		</Row>
+	</TopAppBar>
+	<AutoAdjust {topAppBar}>
+		<Drawer variant="modal" bind:open>
+			<DrawerContent>
+				<List>
+					<Item>
+						<Text>Nothing here yet...</Text>
+					</Item>
+					<Item href="https://www.google.com" target="_blank">
+						<Icon class="material-icons">link</Icon>
+						<Text>Go to Google</Text>
+					</Item>
+				</List>
+			</DrawerContent>
+		</Drawer>
+		<AppContent class="app-content">
+			<Group class="my-4">
+				{#each Object.keys(siteVariants) as site}
+					<Button
+						variant={siteVariants[site]}
+						class="site-button"
+						on:click={() => onClick(site)}
+					>
+						<Label>
+							<img src="/{site}.png" alt="" />
+						</Label>
+					</Button>
 				{/each}
-			</Accordion>				
-		{:else}
-			<div class="loading">
-				<div class="loading__icon"></div>
-				<div class="loading__text">Loading...</div>
+			</Group>
+			<LayoutGrid>
+				<Cell span={4} class="d-flex align-items-center">
+					Filter by country
+				</Cell>
+				<Cell span={8}>
+					<Select
+						bind:value={siteResult.filters.country}
+						on:change={onChange}
+						class="w-100"
+					>
+						<Option value="" selected>🏳️ ----</Option>
+						{#if siteResult.value}
+							{#each [...new Set(siteResult.value.map((r) => r.server.country))] as country}
+								<Option value={country}
+									>{country2emoji(country)} {country}</Option
+								>
+							{/each}
+						{/if}
+					</Select>
+				</Cell>
+			</LayoutGrid>
+			<div class="accordion-container">
+				{#if siteResult.value}
+					<Accordion multiple>
+						{#each siteResult.value as result}
+							{#if siteResult.filters.country === "" || siteResult.filters.country === result.server.country}
+								<Server {result} />
+							{/if}
+						{/each}
+					</Accordion>
+				{:else}
+					<div class="loading">
+						<div class="loading__icon" />
+						<div class="loading__text">Loading...</div>
+					</div>
+				{/if}
 			</div>
-		{/if}
-	</div>
+		</AppContent>
+	</AutoAdjust>
 </main>
 
 <style>
-	main {
+	* :global(.app-content) {
 		max-width: 1000px;
-		margin: 0 auto;
+		margin: 0 auto !important;
 	}
 
 	* :global(.site-button) {
