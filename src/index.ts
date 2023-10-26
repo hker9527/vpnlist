0ff71e0a37ed9501bfbddc5a3d521dae8feb79b6
hker9527 <owo@uwu.cc>
2023-10-26 10:55:53 +0800

    Fix naming

--- src/index.ts
+++ src/index.ts
@@ -116,7 +116,7 @@ const buildRouter = () => {
                 const minute = now.getMinutes();
                 const second = now.getSeconds();
 
-                return `NasuVPN-${ip}-${year}${month.toString().padStart(2, "0")}${date.toString().padStart(2, "0")}-${hour.toString().padStart(2, "0")}${minute.toString().padStart(2, "0")}${second.toString().padStart(2, "0")}-${variant == "current" ? "C" : "L"}${split ? "S" : "O"}.ovpn`;
+                return `NasuVPN-${ip}-${year}${month.toString().padStart(2, "0")}${date.toString().padStart(2, "0")}-${hour.toString().padStart(2, "0")}${minute.toString().padStart(2, "0")}${second.toString().padStart(2, "0")}-${variant == "current" ? "C" : "L"}${typeof split !== "undefined" ? "S" : "O"}.ovpn`;
             };
 
             let config = data.config;
