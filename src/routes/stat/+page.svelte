589a4674f5e1737861c10e7fc08ad64cb24414cc
hker9527 <owo@uwu.cc>
2024-02-13 23:38:19 +0800

    Add world map to stat screen

--- src/routes/stat/+page.svelte
+++ src/routes/stat/+page.svelte
@@ -9,9 +9,13 @@
     } from "@smui/data-table";
     import IconButton from "@smui/icon-button";
     import LinearProgress from "@smui/linear-progress";
+    import { topojson, ChoroplethChart } from "chartjs-chart-geo";
+    import { Chart } from "chart.js/auto";
+    import type { Feature, Geometry } from "geojson";
     import { onMount } from "svelte";
     import { HOST } from "~/lib/const";
     import { CountryCode } from "~/lib/CountryCode";
+    import SitePicker from "~/lib/SitePicker.svelte";
     import {
         ZStatAPIResponse,
         type StatResult,
@@ -22,8 +26,10 @@
 
     let result: (StatResult[number] & {
         total: number;
-        successRate: number
+        successRate: number;
     })[] = [];
+
+    Chart.register();
 
     const fetchStat = async (site = "uma") => {
         result = [];
@@ -32,10 +38,10 @@
         const json = await res.json();
         if (ZStatAPIResponse.check(json)) {
             if (json.success) {
-                result = json.data.map(d => ({
+                result = json.data.map((d) => ({
                     ...d,
                     total: d.success + d.fail,
-                    successRate: (d.success / (d.success + d.fail)) * 100
+                    successRate: (d.success / (d.success + d.fail)) * 100,
                 }));
             } else {
                 prompt(
@@ -69,17 +75,151 @@
         result = result;
     };
 
-    const onClick = async () => {
-        await fetchStat();
+    let countries: Feature<Geometry, { name: string }>[];
+    let map: HTMLCanvasElement;
+    let chart: ChoroplethChart;
+
+    const renderData = async () => {
+        if (chart) {
+            chart.destroy();
+        }
+
+        if (!countries) {
+            const res = await fetch(
+                "https://unpkg.com/world-atlas/countries-50m.json",
+            );
+            const world = await res.json();
+            countries = topojson.feature(
+                world,
+                world.objects.countries as {
+                    type: "GeometryCollection";
+                    geometries: {
+                        type: "Polygon";
+                        arcs: any[];
+                        id: string;
+                        properties: {
+                            name: string;
+                        };
+                    }[];
+                },
+            ).features;
+        }
+
+        const data = countries.map((feature) => ({
+            feature,
+            value:
+                result.find(
+                    (r) =>
+                        new CountryCode(r.country).toFullName() ===
+                        feature.properties!.name,
+                )?.successRate ?? -1,
+        }));
+        chart = new ChoroplethChart(map.getContext("2d")!, {
+            data: {
+                labels: countries.map((feature) => feature.properties!.name),
+                datasets: [
+                    {
+                        label: "Countries",
+                        data,
+                        backgroundColor: data.map((d) => {
+                            if (d.value === -1) {
+                                return "grey";
+                            }
+
+                            const v = d.value / 100;
+
+                            const hex = (x: number) =>
+                                x.toString(16).padStart(2, "0");
+                            const rgb2hex = (rgb: number[]) =>
+                                `#${hex(rgb[0])}${hex(rgb[1])}${hex(rgb[2])}`;
+
+                            const colors = {
+                                safe: [40, 167, 69],
+                                warning: [255, 193, 7],
+                                danger: [220, 53, 69],
+                            };
+
+                            switch (v) {
+                                case 1:
+                                    return rgb2hex(colors.safe);
+                                case 0:
+                                    return rgb2hex(colors.danger);
+                            }
+
+                            const start =
+                                v >= 0.5 ? colors.warning : colors.danger;
+                            const end = v >= 0.5 ? colors.safe : colors.warning;
+
+                            // Map the two portions [0,0.5) and (0.5,1] into [0,1]
+                            const ratio = (v % 0.5) * 2;
+
+                            const interpolateColorComponent = (
+                                start: number,
+                                end: number,
+                            ) => {
+                                return Math.ceil(
+                                    start * (1 - ratio) + end * ratio,
+                                );
+                            };
+
+                            const r = interpolateColorComponent(
+                                start[0],
+                                end[0],
+                            );
+                            const g = interpolateColorComponent(
+                                start[1],
+                                end[1],
+                            );
+                            const b = interpolateColorComponent(
+                                start[2],
+                                end[2],
+                            );
+
+                            return rgb2hex([r, g, b]);
+                        }),
+                    },
+                ],
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                scales: {
+                    projection: {
+                        axis: "x",
+                        projection: "naturalEarth1",
+                    },
+                    color: {
+                        axis: "x",
+                        display: false
+                    }
+                }
+            },
+        });
+    };
+
+    const onSwitchSite = async (site: string) => {
+        await fetchStat(site);
         onSort();
+        await renderData();
     };
 
     onMount(async () => {
-        await onClick();
+        await fetchStat();
+        onSort();
+        await renderData();
     });
 </script>
 
 <main>
+    <div class="d-flex justify-content-center">
+        <SitePicker onClick={onSwitchSite} />
+    </div>
+
+    <canvas id="map" class="my-2" bind:this={map}></canvas>
+
     <DataTable
         sortable
         bind:sort
