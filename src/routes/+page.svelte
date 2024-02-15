89e60dac31fd590aab1110665aeca27837f46f21
hker9527 <owo@uwu.cc>
2024-02-15 10:55:10 +0800

    Add take & orderBy filters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Accordion from "@smui-extra/accordion";
 	import Select, { Option } from "@smui/select";
+	import TextField from "@smui/textfield";
 	import { onMount } from "svelte";
 	import SitePicker from "~/lib/SitePicker.svelte";
 	import Server from "~/lib/Server.svelte";
@@ -10,29 +11,25 @@
 		type SiteResult,
 	} from "~/lib/types/api/SiteAPIResponse";
     import { CountryCode } from "~/lib/CountryCode";
+    import Button from "@smui/button";
 
-	let siteResult: {
-		value: SiteResult | null;
-		updatedAt: number;
-		filters: {
-			country: string;
-		};
-	} = {
-		value: null,
-		updatedAt: 0,
-		filters: {
-			country: "",
-		},
+	const options = {
+		site: "uma",
+		country: "",
+		take: 20,
+		orderBy: "timestamp"
 	};
 
-	const fetchResult = async (site: string) => {
-		siteResult.value = null;
+	let siteResult: SiteResult | null = null;
 
-		const res = await fetch(`${HOST}/api/site/${site}`);
+	const fetchResult = async () => {
+		siteResult = null;
+
+		const res = await fetch(`${HOST}/api/site/${options.site}?take=${options.take}&orderBy=${options.orderBy}`);
 		const json = await res.json();
 		if (ZSiteAPIResponse.check(json)) {
 			if (json.success) {
-				siteResult.value = json.data;
+				siteResult = json.data;
 			} else {
 				prompt("Failed to fetch data from the server.\nServer response:", JSON.stringify(json));
 			}
@@ -41,18 +38,17 @@
 		}
 	};
 
-	let site = "uma";
 	const onClick = async (_site: string) => {
-		site = _site;
-		await fetchResult(site);
+		options.site = _site;
+		await fetchResult();
 	};
 
 	onMount(async () => {
-		await fetchResult(site);
+		await fetchResult();
 	});
 
 	const onChange = async () => {
-		await fetchResult(site);
+		await fetchResult();
 	};
 </script>
 
@@ -60,20 +56,45 @@
 	<div class="border rounded p-4 mb-2">
 		Filters:
 		<Select
-			bind:value={siteResult.filters.country}
-			on:change={onChange}
+			bind:value={options.country}
 			class="w-100"
 			label="Country"
 		>
 			<Option value={null} />
-			{#if siteResult.value}
-				{#each [...new Set(siteResult.value.map((r) => r.country))] as country}
+			{#if siteResult}
+				{#each [...new Set(siteResult.map((r) => r.country))] as country}
 					<Option value={country}>
 						{new CountryCode(country).toString()}
 					</Option>
 				{/each}
 			{/if}
 		</Select>
+
+		<TextField
+			bind:value={options.take}
+			class="w-100"
+			label="Result count"
+			input$min="1"
+			input$max="100"
+			input$step="10"
+		/>
+
+		<Select
+			bind:value={options.orderBy}
+			class="w-100"
+			label="Order by"
+		>
+			<Option value="timestamp" selected>Most recent</Option>
+			<Option value="duration">Lowest ping</Option>
+			<Option value="speed">Fastest speed</Option>
+		</Select>
+
+		<Button
+			class="w-100"
+			on:click={() => onChange()}
+		>
+			Apply
+		</Button>
 	</div>
 
 	<div class="d-flex justify-content-center my-1">
@@ -81,11 +102,11 @@
 	</div>
 
 	<div class="accordion-container">
-		{#if siteResult.value}
-			{#if siteResult.value.length > 0}
+		{#if siteResult}
+			{#if siteResult.length > 0}
 				<Accordion multiple>
-					{#each siteResult.value as result}
-						{#if !siteResult.filters.country || siteResult.filters.country === result.country}
+					{#each siteResult as result}
+						{#if !options.country || options.country === result.country}
 							<Server {result} />
 						{/if}
 					{/each}
