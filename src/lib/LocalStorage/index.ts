1d3832ac9055b688a6a16ba81c790522c35ecc6a
hker9527 <owo@uwu.cc>
2025-12-08 21:29:39 +0800

    Lint

--- src/lib/LocalStorage/index.ts
+++ src/lib/LocalStorage/index.ts
@@ -2,46 +2,46 @@ import { BaseMigration } from "./migrations";
 import { type Shape, defaultSettings, Migration as Migration001 } from "./migrations/001_init";
 
 const migrations = [
-  new Migration001()
+	new Migration001()
 ] as BaseMigration[];
 
 export class Settings {
-  #settings!: Shape;
+	#settings!: Shape;
 
-  constructor() {
-    if (typeof localStorage === "undefined") {
-      this.#settings = defaultSettings;
-      return;
-    }
+	constructor() {
+		if (typeof localStorage === "undefined") {
+			this.#settings = defaultSettings;
+			return;
+		}
 
-    const _version = localStorage.getItem("version") ?? "0";
-    const _settings = localStorage.getItem("settings") ?? "{}";
+		const _version = localStorage.getItem("version") ?? "0";
+		const _settings = localStorage.getItem("settings") ?? "{}";
 
-    let version = parseInt(_version, 10);
-    let settings = JSON.parse(_settings);
+		let version = parseInt(_version, 10);
+		let settings = JSON.parse(_settings);
 
-    for (const migration of migrations) {
-      if (migration.version > version) {
-        settings = migration.up(settings);
-        version = migration.version;
-      }
-    }
+		for (const migration of migrations) {
+			if (migration.version > version) {
+				settings = migration.up(settings);
+				version = migration.version;
+			}
+		}
 
-    this.#settings = settings;
-    this.#save();
-  }
+		this.#settings = settings;
+		this.#save();
+	}
 
-  #save() {
-    localStorage.setItem("version", migrations[migrations.length - 1].version.toString());
-    localStorage.setItem("settings", JSON.stringify(this.#settings));
-  }
+	#save() {
+		localStorage.setItem("version", migrations[migrations.length - 1].version.toString());
+		localStorage.setItem("settings", JSON.stringify(this.#settings));
+	}
 
-  load(): Shape {
-    return this.#settings;
-  }
+	load(): Shape {
+		return this.#settings;
+	}
 
-  save(settings: Shape) {
-    this.#settings = settings;
-    this.#save();
-  }
+	save(settings: Shape) {
+		this.#settings = settings;
+		this.#save();
+	}
 };
